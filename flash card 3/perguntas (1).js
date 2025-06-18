07100fcb02b78861a5b668018fa66d6398ce160a
luizdoplay <gluizguilherme630@gmail.com>
2025-06-18 03:22:36 -0300

    Add files via upload

---
+++ flash card 3/perguntas (1).js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Quem foi o líder da União Soviética durante a maior parte da Segunda Guerra Mundial?',
+    'Joseph Stálin'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal objetivo das Grandes Navegações realizadas por Portugal e Espanha no século XV e XVI?',
+    'Buscar novas rotas comerciais e riquezas'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Industrial e em que país ela começou?',
+    'Transformação econômica com uso de máquinas'
+)
+
+criaCartao(
+    'História',
+    'Qual império antigo ficou conhecido por seu poderoso exército e por conquistar vastas regiões da Europa, Ásia e África?',
+    'Império Romano'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a importância da assinatura da Lei Áurea no Brasil em 1888?',
+    'Fim oficial da escravidão no Brasil'
+)
+
+criaCartao(
+    'História',
+    'O que motivou a queda da Bastilha em 14 de julho de 1789, marco inicial da Revolução Francesa?',
+    'Revolta contra a monarquia e a desigualdade social'
+)
